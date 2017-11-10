63033cc05bd7c2ba750310f88e7cfde32f03b3ec
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-10 13:31:23 -0500

    Initial Commit

---
+++ CPP-PRG-11-13-Drink-Machine-Simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-PRG-11-13-Drink-Machine-Simulator
+//
+//  Created by Keith Smith on 11/10/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
