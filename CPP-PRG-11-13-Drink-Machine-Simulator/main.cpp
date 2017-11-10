19ab39aaa07b7e1f55bce041caaaee61a29051ec
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-10 13:36:23 -0500

    Prompt written.

--- CPP-PRG-11-13-Drink-Machine-Simulator/main.cpp
+++ CPP-PRG-11-13-Drink-Machine-Simulator/main.cpp
@@ -5,11 +5,42 @@
 //  Created by Keith Smith on 11/10/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that simulates a soft drink machine. The program should use a structure
+//  that stores the following data:
+//
+//      Drink Name
+//      Drink Cost
+//      Number of Drinks in Machine
+//
+//  The program should create an array of five structures. The elements should be
+//  initialized with the following data:
+//
+//      Drink Name          Cost            Number in Machine
+//      -----------------------------------------------------
+//      Cola                .75             20
+//      Root Beer           .75             20
+//      Lemon-Lime          .75             20
+//      Grape Soda          .80             20
+//      Cream Soda          .80             20
+//
+//  Each time the program runs, it should enter a loop that performs the following steps:
+//  A list of drinks is displayed on the screen. The user should be allowed to either quit the
+//  program or pick a drink. If the user selects a drink, he or she will next enter the amount
+//  of money that is to be inserted into the drink machine. The program should display the
+//  amount of change that would be returned, and subtract one from the number of that
+//  drink left in the machine. If the user selects a drink that has sold out, a message should
+//  be displayed. The loop then repeats. When the user chooses to quit the program, it
+//  should display the total amount of money the machine earned.
+//
+//  Input Validation: When the user enters an amount of money, do not accept negative
+//  values or values greater than $1.00.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
